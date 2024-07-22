c3ce911809b5ad489ba3cc8153bfcc97b4645d3a
Myroniuk Ilona <knopkakyrs@gmail.com>
2024-07-22 19:12:32 +0300

    added project "Guess my number"

--- js/project-modals.js
+++ js/project-modals.js
@@ -1,31 +1,63 @@
-const projectModalFullStack = document.querySelector('#personal-project-full-stack');
-const projectModalWeddingProject = document.querySelector('#personal-project-wedding-project');
-const projectModaProTest = document.querySelector('#personal-project-pro-test');
-const projectModalProjectGoit = document.querySelector('#personal-project-goit');
+const projectModalGuessMyNumber = document.querySelector(
+  "#personal-project-guess_my_number"
+);
+const projectModalWeddingProject = document.querySelector(
+  "#personal-project-wedding-project"
+);
+const projectModaProTest = document.querySelector("#personal-project-pro-test");
+const projectModalProjectGoit = document.querySelector(
+  "#personal-project-goit"
+);
 
-const projectModalQuestify = document.querySelector('#team-project-questify');
-const projectModalItTest = document.querySelector('#team-project-it-test');
+const projectModalQuestify = document.querySelector("#team-project-questify");
+const projectModalItTest = document.querySelector("#team-project-it-test");
 
-const projectOpenBtnFullStack = document.querySelector('#personal-project-full-stack-btn');
-const projectOpenBtnWeddingProject = document.querySelector('#personal-project-wedding-project-btn');
-const projectOpenBtnProTest = document.querySelector('#personal-project-pro-test-btn');
-const projectOpenBtnProjectGoit = document.querySelector('#personal-project-goit-btn');
+const projectOpenBtnGuessMyNumber = document.querySelector(
+  "#personal-project-guess_my_number-btn"
+);
+const projectOpenBtnWeddingProject = document.querySelector(
+  "#personal-project-wedding-project-btn"
+);
+const projectOpenBtnProTest = document.querySelector(
+  "#personal-project-pro-test-btn"
+);
+const projectOpenBtnProjectGoit = document.querySelector(
+  "#personal-project-goit-btn"
+);
 
-const projectOpenBtnQuestify = document.querySelector('#team-project-questify-btn');
-const projectOpenBtnItTest = document.querySelector('#team-project-it-test-btn');
+const projectOpenBtnQuestify = document.querySelector(
+  "#team-project-questify-btn"
+);
+const projectOpenBtnItTest = document.querySelector(
+  "#team-project-it-test-btn"
+);
 
-const projectModals = [ projectModalFullStack, projectModalWeddingProject, projectModaProTest, projectModalProjectGoit, projectModalQuestify, projectModalItTest];
-const projectBtns = [ projectOpenBtnFullStack, projectOpenBtnWeddingProject, projectOpenBtnProTest, projectOpenBtnProjectGoit, projectOpenBtnQuestify, projectOpenBtnItTest];
+const projectModals = [
+  projectModalGuessMyNumber,
+  projectModalWeddingProject,
+  projectModaProTest,
+  projectModalProjectGoit,
+  projectModalQuestify,
+  projectModalItTest,
+];
+const projectBtns = [
+  projectOpenBtnGuessMyNumber,
+  projectOpenBtnWeddingProject,
+  projectOpenBtnProTest,
+  projectOpenBtnProjectGoit,
+  projectOpenBtnQuestify,
+  projectOpenBtnItTest,
+];
 
 projectBtns.forEach((btn, index) => {
-    const projectModal = projectModals[index];
+  const projectModal = projectModals[index];
 
-    if (btn) {
-        btn.addEventListener('click', (event) => {
-            event.preventDefault();
-            projectModal.classList.add(MODAL_ACTIVE_CLASS);
+  if (btn) {
+    btn.addEventListener("click", (event) => {
+      event.preventDefault();
+      projectModal.classList.add(MODAL_ACTIVE_CLASS);
 
-            document.body.classList.add(BODY_SCROLL_DISABLE_CLASS);
-        })
-    }
+      document.body.classList.add(BODY_SCROLL_DISABLE_CLASS);
+    });
+  }
 });
